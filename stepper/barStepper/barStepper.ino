9cd0db9b8c5f3fe73e687465bed231421384b8dc
Vaibhav Viswanathan <omahavaibhav@yahoo.com>
2015-10-24 01:30:46 -0700

    Fixed stepper bugs
    Changed timing on stepper

--- stepper/barStepper/barStepper.ino
+++ stepper/barStepper/barStepper.ino
@@ -7,46 +7,49 @@ const int stepsPerBigRevolution = 400; //rotates the big gear one revolution
 
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8,9,10,11);
+Stepper myStepper(stepsPerLittleRevolution, 8,9,10,11);
 
 // set global state
 int state;
 
 void setup(){
-  // set the speed at 30 rpm
-  myStepper.setSpeed(30);
+  // set the speed at 10 rpm
+  myStepper.setSpeed(7.5);
   //initialize serial port
   Serial.begin(9600);
   //define pins  
   pinMode(7, INPUT);
+  Serial.println("start");
   //sends to reset state
-  reset();  
+//  reset();  
 }
 
 void loop(){
   if (Serial.available()){ //checks to see if there is a reading from the pid
     // read in from serial
-    char piRead = Serial.read(); 
+    int piRead = Serial.read()-'0'; 
     // check if reset
+    Serial.println(piRead);
     if (piRead == 69){
        reset(); 
     }
     // check if state changing
-    else if (piRead == (1 | 2 | 3 | 4 | 5 | 6 )){
+    else if (piRead > 0 && piRead < 7)
+    {
        changeState(piRead);
     }
     }
   }
-}
+
 
 //moves the turntable to given state
 void changeState(int newState){
   // number of states to rotate
   int stateSwitch = state - newState;
-  // number of steps to next step
+  // number of steps to next state
   int numSteps = (stepsPerBigRevolution/6)*stateSwitch;
   // Rotate to next state
-  myStepper.step(numSteps);
+  myStepper.step(numSteps-0);
   // Set the state
   state = newState;
   // Notify the Pi
@@ -66,7 +69,6 @@ void reset(){
     myStepper.step(1);
     delay(50); //control speed of arduino (approx 7.5 RPM)
     }
-  }
  //sets the state
- state = 0; 
+ state = 1; 
 }
